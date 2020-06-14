ae62013e3ec83db0cf2d6899c5b9b8e73c04aed3
NTUMBABU <tshiteyababci@gmail.com>
2020-06-14 05:15:23 +0400

    Ajout de la fonction aleatoire

--- script/plus_moin.c
+++ script/plus_moin.c
@@ -1,1 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 
+void plus_moins(int choix_joueur, int valeur_a_touver)
+{
+	int x = 0;
+	do
+	{	
+		scanf("%d",&choix_joueur);
+		if(choix_joueur == valeur_a_touver)
+		{
+			printf("BINGO\n");
+		}
+		
+		else if(choix_joueur < valeur_a_touver)
+		{
+			printf("PLUS\n");
+		}
+
+		else
+		{
+			printf("MOINS\n");
+		}
+	}while(choix_joueur != valeur_a_touver);	
+}
+
+int main()
+{
+	srand(time(NULL));
+	int nbr_secret = rand()%11;
+	int choix;
+	//int resultat = 0;
+	printf("deviner un nombre compris entre 0 et 10 : "); plus_moins(choix, nbr_secret);
+	printf("\n");
+	return 0;
+}
